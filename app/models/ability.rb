d8158008b120027b4d03fef607f3a97d5fd9f996
awoka333 <na23okzw.htre@gmail.com>
2020-12-25 17:35:34 +0000

    [add]user routes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,13 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
+      user ||= User.new # guest user (not logged in)
       if user.admin?
         can :manage, :all
-      else
+      elsif user.member?
         can :read, :all
+      else
+        can :read, :all # guest user
       end
     #
     # The first argument to `can` is the action you are giving the user
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get 'homes/about'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only:[:index, :edit, :update]
+  get 'users/my_page/:id' => 'users#show', as: 'my_page'
+  get 'users/unsubscribe'
+  patch 'users/withdraw'
 end
