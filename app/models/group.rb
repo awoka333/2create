52dc7ba5658a5b1f6495031f35cf1f321b2968e3
awoka333 <na23okzw.htre@gmail.com>
2021-01-04 13:21:42 +0000

    [Fix]enum conflict resolved

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ApplicationRecord
   belongs_to :user
   belongs_to :activity
 
-  enum member_status: { '承認待ち': 0, 'メンバー': 1, 'リーダー': 2, 'シニア': 3 }, _prefix: true
-  enum graduate_status: { '卒業しない':0, '卒業依頼':1, 'シニア':2 }, _prefix: true
+  enum member_status: { '承認待ち': 0, 'メンバー': 1, 'リーダー': 2, 'シニア': 3 }
+  enum graduate_status: { '卒業しない':0, '卒業依頼':1, '卒業':2 }
 end
